418e7a25ee398db708f171d0e7595ef8b30d2c1c
David Goodman <dagoodma@gmail.com>
2018-09-12 16:56:34 -0700

    Added test for secrets webhook alert channel.

--- slackwrap_test/secrets_test.go
+++ slackwrap_test/secrets_test.go
@@ -2,13 +2,24 @@ package slackwrap_test
 
 import (
 	"bitbucket.org/dagoodma/nancyhillis-go/slackwrap"
+	"strings"
 	"testing"
 )
+
+func TestGetAlertsChannel(t *testing.T) {
+	alertsChannel := slackwrap.GetAlertsChannel()
+	if len(alertsChannel) < 1 {
+		t.Errorf("GetAlertsChannel() == blank, want non-empty string")
+	}
+	if !strings.HasPrefix(alertsChannel, "#") {
+		t.Errorf("GetAlertsChannel() == blank, string starting with '#'")
+	}
+}
 
 func TestGetApiToken(t *testing.T) {
 	apiToken := slackwrap.GetApiToken()
 	if len(apiToken) < 1 {
-		t.Errorf("GetApiToken() == blank , want non-empty string")
+		t.Errorf("GetApiToken() == blank, want non-empty string")
 	}
 }
 
